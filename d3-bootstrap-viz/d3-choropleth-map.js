5ad933e2ca53f9efa407d3c37343524f941ebbb3
hierros <lukxas@gmail.com>
2020-10-29 00:54:15 -0400

    choropleth updated with year and ses dropdown

--- d3-bootstrap-viz/d3-choropleth-map.js
+++ d3-bootstrap-viz/d3-choropleth-map.js
@@ -17,13 +17,13 @@ var color = d3.scaleQuantile().range(d3.schemeBlues[4])
 // enter code to define tooltip
 var tip = d3.tip()
     .attr("class", "d3-tip")
-    .html(function(d) { return "County: " + d.properties.NAME + "</br>Game: " + d.Game + "</br>Avg Rating: " + d.average_rating + "</br>Number of Users: " + d.number_of_users; })
-    .offset([50,0])
+    .html(function(d) { return "County: " + d.properties.NAME + "<br />Graduation Rate: "  + d.grad_rate; })
+    .offset([50,-120])
 
 // enter code to define projection and path required for Choropleth
 var projection = d3.geoMercator()
         .scale(5650)
-        .center([-85, 42.85])
+        .center([-85, 42.88])
         .rotate([-10, 0])
         .translate([w/2, h/2])
 
@@ -35,12 +35,25 @@ var path = d3.geoPath().projection(projection)
 Promise.all([
     // enter code to read files
     d3.json('NY-36-new-york-counties.json'),
-    d3.dsv(",", "ratings-by-country.csv", function(d) {
+    d3.dsv(",", "df_grad_rate_pct_county.csv", function(d) {
         return {
-            Game: d.Game,
-            Country: d.Country,
-            number_of_users: +d["Number of Users"],
-            average_rating: +d["Average Rating"]
+            'COUNTY_NAME': d.COUNTY_NAME,
+            'AMERICAN INDIAN OR ALASKA NATIVE': +d['AMERICAN INDIAN OR ALASKA NATIVE'],
+            'ASIAN OR PACIFIC ISLANDER': +d['ASIAN OR PACIFIC ISLANDER'],
+            'BLACK': +d['BLACK'],
+            'ECONOMICALLY DISADVANTAGED' : +d['ECONOMICALLY DISADVANTAGED'],
+            'FEMALE' : +d['FEMALE'],
+            'FORMERLY LIMITED ENGLISH PROFICIENT' : +d['FORMERLY LIMITED ENGLISH PROFICIENT'],
+            'GENERAL EDUCATION STUDENTS' : +d['GENERAL EDUCATION STUDENTS'],
+            'HISPANIC OR LATINO' : +d['HISPANIC OR LATINO'],
+            'LIMITED ENGLISH PROFICIENT' : +d['LIMITED ENGLISH PROFICIENT'],
+            'MALE' : +d['MALE'],
+            'MIGRANT' : +d['MIGRANT'],
+            'MULTIRACIAL' : +d['MULTIRACIAL'],
+            'STUDENTS WITH DISABILITIES' : +d['STUDENTS WITH DISABILITIES'],
+            'WHITE' : +d['WHITE'],
+            'PCT_GRAD' : +d['PCT_GRAD'],
+            'YEAR' : +d['YEAR']
         } 
     })
 ]).then(function (data, error) {
@@ -53,42 +66,63 @@ Promise.all([
 // world: topojson from world_countries.json
 // gameData: data from ratings-by-country.csv
 
-function ready(error, ny, gameData) {
-
-
-var game = []
-
-
+function ready(error, ny, schoolData) {   
+    var k = Object.keys(schoolData[0])
+    k.shift()
+    k.pop()
+    k.sort()
     
-   
+    // enter code to append the game options to the dropdown
+    var select = document.getElementById("ses")
+    for(var i = 0; i < k.length; i++) {
+        var game = k[i]
+        var el = document.createElement("option")
+        el.textContent = game
+        el.value = game
+        select.appendChild(el)
+    }
     
+    // event listener for the dropdown. Update choropleth and legend when selection changes. Call createMapAndLegend() with required arguments.
+        var s = document.getElementById("ses")
+        var y = document.getElementById("year")
+		s.addEventListener("change", function() {
+            createMapAndLegend(ny, schoolData, this.value, y.value)})
+
+        y.addEventListener("change", function() {
+            createMapAndLegend(ny, schoolData, s.value, this.value)})
+
     // create Choropleth with default option. Call createMapAndLegend() with required arguments. 
-    createMapAndLegend(ny, gameData, game[0])
+    createMapAndLegend(ny, schoolData, 'AMERICAN INDIAN OR ALASKA NATIVE', 2015)
 }
 
 // this function should create a Choropleth and legend 
-function createMapAndLegend(ny, gameData, selectedGame){ 
+function createMapAndLegend(ny, schoolData, selectedSES, selectedYear){ 
     svg.selectAll(".new-york").remove()
-    
-    var data =[]
-    gameData.forEach(function(d) {
-        if (d.Game == selectedGame) {
-            data.push(d)
-        }
-    })
 
-    color.domain([0,100])
+    var data = schoolData.filter(x => x.YEAR==selectedYear)
+
+    color.domain([d3.min(data, function(d){return d[selectedSES];}), 
+        d3.max(data, function(d){return d[selectedSES];})])
     
     svg.append("g")
-        .attr("class", "countries")
+        .attr("class", "new-york")
         .selectAll("path")
         .data(topojson.feature(ny, ny.objects.cb_2015_new_york_county_20m).features)
         .enter()
         .append("path")
         .attr("d", path)
         .attr("fill", function(d) {
-            console.log(d.properties.NAME)
-            return color(Math.floor(Math.random() * Math.floor(100)))
+            var county = (d.properties.NAME).toUpperCase()
+            data.forEach(function(e) {
+                if (e.COUNTY_NAME == county) {
+                    d.grad_rate = e[selectedSES]
+                }
+            })
+            if (d.grad_rate != null) {
+                return color(d.grad_rate)
+            } else {
+                return "gray"
+            }
          })
         .on('mouseover', tip.show)
         .on('mouseout', tip.hide)
